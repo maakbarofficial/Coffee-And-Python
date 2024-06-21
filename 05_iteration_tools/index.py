d703b66c6b7c86edfecc2b249cc7506e8390f129
Muhammad Ali Akbar <maakbarofficial@gmail.com>
2024-06-21 13:10:08 +0500

    iteration concept

---
+++ 05_iteration_tools/index.py
@@ -0,0 +1,25 @@
+import time
+
+print("This is index file")
+
+username = "user one"
+
+print(username)
+
+# f = open('index.py')
+# for line in open('index.py'):
+#   print(line)
+
+# while True:
+#     line = f.readline()
+#     if not line:
+#         break
+#     print(line, end="")
+
+# myList = [1, 2, 3, 4, 5]
+# I = iter(myList)
+# I.__next__()
+
+# f = open('index.py')
+# iter(f) is f
+# iter(f) is f.__iter__()
