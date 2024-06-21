35697f41f10cf59c992c014fccf5264979b1fe6b
Muhammad Ali Akbar <maakbarofficial@gmail.com>
2024-06-21 20:25:28 +0500

    another example of scope

--- 07_scopes/index.py
+++ 07_scopes/index.py
@@ -40,3 +40,17 @@ def f1():
 
 myresult = f1()
 myresult()
+
+
+def coder(num):
+    def actual(x):
+        return x**num
+
+    return actual
+
+
+f = coder(2)
+g = coder(3)
+
+print(f(3))
+print(g(3))
